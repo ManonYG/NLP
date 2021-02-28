1d060d63c19ce74a554a99ee175ca53157df6a5f
MGiraud <manon.giraud@gmail.com>
2021-02-28 17:53:00 +0100

    Création du fichier HTML

--- API/main.py
+++ API/main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("ihm.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

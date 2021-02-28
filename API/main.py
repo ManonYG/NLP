d3ee8ef5903f415612d13aabc8e0ca1f0956a415
MGiraud <manon.giraud@gmail.com>
2021-02-28 19:03:47 +0100

    Création de la méthode POST

--- API/main.py
+++ API/main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/", methods=('GET', 'POST'))
+def main():
+    if request.method == 'POST':
+        texte = request.form['texte']
     return render_template("ihm.html")
 
 if __name__ == "__main__":
